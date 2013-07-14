d9623d8f7f400a2c29470cc42e6ba19e2f674264
Chris Moore <christopher.e.moore@gmail.com>
2013-07-14 15:35:09 -0600

    mipmap on non-fbo, fixed input

--- main.js
+++ main.js
@@ -326,13 +326,13 @@ $(document).ready(function(){
 
 	var skyTex = new GL.TextureCube({
 		flipY : true,
-		/*
 		generateMipmap : true,
 		magFilter : gl.LINEAR,
 		minFilter : gl.LINEAR_MIPMAP_LINEAR,
-		*/
+		/*
 		magFilter : gl.LINEAR,
 		minFilter : gl.NEAREST,
+		*/
 		wrap : {
 			s : gl.CLAMP_TO_EDGE,
 			t : gl.CLAMP_TO_EDGE
@@ -415,7 +415,7 @@ $(document).ready(function(){
 		pressObj : canvas,
 		move : function(dx,dy) {
 			var rotAngle = Math.PI / 180 * .01 * Math.sqrt(dx*dx + dy*dy);
-			quat.setAxisAngle(tmpQ, [-dy, dx, 0], rotAngle);
+			quat.setAxisAngle(tmpQ, [dy, dx, 0], rotAngle);
 
 			quat.mul(GL.view.angle, GL.view.angle, tmpQ);
 			quat.normalize(GL.view.angle, GL.view.angle);
