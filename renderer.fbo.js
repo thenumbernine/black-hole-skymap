426b54bea2a8fd4980518179f40c9229f375685f
Christopher Moore <christopher.e.moore@gmail.com>
2019-04-07 17:36:58 +0700

    fixing kerr

--- renderer.fbo.js
+++ renderer.fbo.js
@@ -67,56 +67,62 @@ GeodesicFBORenderer = makeClass({
 				['Schwarzschild Black Hole'] : mlstr(function(){/*
 uniform float blackHoleMass;
 
+//common variables used by 
+//* initialization of g_ab (in the vertex shader reset)
+//* update of x''^a = -Gamma^a_bc x'^b x'^c (in the fragment shader update)
+struct metricInfo_t {
+	float inv_r;
+	float _1_plus_m_2r;
+	float _1_minus_m_2r;
+	float _1_plus_m_2r_sq;
+	float _1_plus_m_2r_toTheFourth;
+};
+
+metricInfo_t init_metricInfo(vec4 pos) {
+	float r = length(pos.xyz);
+	metricInfo_t m;
+	m.inv_r = 1. / r;
+	float _m_2r = .5 * blackHoleMass * m.inv_r;
+	m._1_minus_m_2r = 1. - _m_2r;
+	m._1_plus_m_2r = 1. + _m_2r;
+	m._1_plus_m_2r_sq = m._1_plus_m_2r * m._1_plus_m_2r;
+	m._1_plus_m_2r_toTheFourth = m._1_plus_m_2r_sq * m._1_plus_m_2r_sq;
+	return m;
+}
+
 // g_tt = -(1 - m/(2r))^2 / (1 + m/(2r))^2
-float g_tt(vec3 pos) {
-	float r = length(pos);
-	float _m_2r = blackHoleMass / (2. * r);
-	float _1_plus_m_2r = 1. + _m_2r;
-	float _1_minus_m_2r = 1. - _m_2r;
-	float ratio = _1_minus_m_2r / _1_plus_m_2r;
-	return -ratio * ratio; 
+float g_tt(metricInfo_t m) {
+	float ratio = m._1_minus_m_2r / m._1_plus_m_2r;
+	return -ratio * ratio;
 }
 
 // g_ti = 0
-vec3 g_ti(vec3 pos) { 
+vec3 g_ti(metricInfo_t m) { 
 	return vec3(0., 0., 0.); 
 }
 
-// g_ij = delta_ij (1 + m/(2r))^4 
-mat3 g_ij(vec3 pos) {
-	float r = length(pos);
-	float _m_2r = blackHoleMass / (2. * r);
-	float _1_plus_m_2r = 1. + _m_2r;
-	float _1_plus_m_2r_sq = _1_plus_m_2r * _1_plus_m_2r;
-	float _1_plus_m_2r_toTheFourth = _1_plus_m_2r_sq * _1_plus_m_2r_sq;
-	return mat3(_1_plus_m_2r_toTheFourth);
+// g_ij = delta_ij (1 + m/(2r))^4
+mat3 g_ij(metricInfo_t m) {
+	return mat3(m._1_plus_m_2r_toTheFourth);
 }
 
-vec4 accel(vec4 pos, vec4 vel) {
-	float r = length(pos.xyz);
-	float posDotVel = dot(pos.xyz, vel.xyz);
-	float posDotVelSq = posDotVel * posDotVel;
+vec4 accel(metricInfo_t m, vec4 pos, vec4 vel) {
+	float inv_r = m.inv_r;
+	float x_dot_v = dot(pos.xyz, vel.xyz);
 	float velSq = dot(vel.xyz, vel.xyz);
 	float R = 2. * blackHoleMass;
-	float r2 = r * r;
-	float r3 = r * r2;
-
-
-	float _m_2r = blackHoleMass / (2. * r);
-	float _1_plus_m_2r = 1. + _m_2r;
-	float _1_minus_m_2r = 1. - _m_2r;
+	float inv_r2 = inv_r * inv_r;
+	float inv_r3 = inv_r * inv_r2;
 	
-	float _1_plus_m_2r_sq = _1_plus_m_2r * _1_plus_m_2r;
-	float _1_plus_m_2r_tothe3 = _1_plus_m_2r_sq * _1_plus_m_2r;
-	float _1_plus_m_2r_tothe6 = _1_plus_m_2r_tothe3 * _1_plus_m_2r_tothe3;
+	float _1_plus_m_2r_tothe6 = m._1_plus_m_2r_sq * m._1_plus_m_2r_toTheFourth;
 	
 	vec4 result;
-	result.w = -blackHoleMass * 2. * vel.w * posDotVel / (_1_plus_m_2r * _1_minus_m_2r * r3);
+	result.w = -blackHoleMass * 2. * vel.w * x_dot_v * inv_r3 / (m._1_plus_m_2r * m._1_minus_m_2r);
 	result.xyz = -blackHoleMass * (
-		vel.w * vel.w * pos.xyz * _1_minus_m_2r / _1_plus_m_2r_tothe6 
-		- 2. * vel.xyz * posDotVel
-		+ velSq * pos.xyz 
-	) / (_1_plus_m_2r * r3);
+		vel.w * vel.w * pos.xyz * m._1_minus_m_2r / _1_plus_m_2r_tothe6
+		- 2. * vel.xyz * x_dot_v
+		+ velSq * pos.xyz
+	) * inv_r3 / m._1_plus_m_2r;
 	return result;
 }
 
@@ -130,28 +136,39 @@ uniform float blackHoleMass;
 uniform float blackHoleCharge;
 uniform float blackHoleAngularMomentum;
 
-float g_tt(vec3 pos) {
-	return -1. + 2. * blackHoleMass / length(pos.xyz);
+struct metricInfo_t {
+	vec3 pos;
+	float inv_r;
+};
+
+metricInfo_t init_metricInfo(vec4 pos) {
+	metricInfo_t m;
+	m.pos = pos.xyz;
+	float r = length(pos.xyz);
+	m.inv_r = 1. / r;
+	return m;
 }
 
-vec3 g_ti(vec3 pos) { 
-	float R = 2. * blackHoleMass;
-	return R * pos.xyz / dot(pos.xyz, pos.xyz);
+float g_tt(metricInfo_t m) {
+	return -1. + 2. * blackHoleMass * m.inv_r;
 }
 
-mat3 g_ij(vec3 pos) {
+vec3 g_ti(metricInfo_t m) { 
+	return 2. * blackHoleMass * m.inv_r * m.inv_r * m.pos;
+}
+
+mat3 g_ij(metricInfo_t m) {
 	float R = 2. * blackHoleMass;
-	float inv_r = 1. / length(pos.xyz);
-	float R_r = R * inv_r;
-	vec3 npos = pos.xyz * inv_r;	
+	float R_r = R * m.inv_r;
+	vec3 npos = m.pos.xyz * m.inv_r;	
 	return mat3(1.) + R_r * outerProduct(npos, npos);
 }
 
-vec4 accel(vec4 pos, vec4 vel) {
+vec4 accel(metricInfo_t m, vec4 pos, vec4 vel) {
 	float R = 2. * blackHoleMass;
 	
-	float inv_r = 1. / length(pos.xyz);
-	vec4 npos = pos * inv_r;	
+	float inv_r = m.inv_r;
+	vec4 npos = pos * inv_r;
 	vec4 nvel = vel * inv_r;
 
 	float R_r = R * inv_r;
@@ -190,6 +207,60 @@ dr/dx = r x / (2 r^2 + a^2 - x^2 - y^2 - z^2)
 dr/dy = r y / (2 r^2 + a^2 - x^2 - y^2 - z^2)
 dr/dz = (r + a^2/r) z / (2 r^2 + a^2 - x^2 - y^2 - z^2)
 r_,i = (r + delta_iz a^2 / r) / (2 r^2 + a^2 - x^2 - y^2 - z^2) x^i
+
+
+l_t = 1
+l_x = (r x + a y) / (r^2 + a^2)
+l_y = (r y - a x) / (r^2 + a^2)
+l_z = z / r
+
+l_t,u = 0
+l_x,x = ((r,x x + r) (r^2 + a^2) - (r x + a y) (2 r r,x)) / (r^2 + a^2)^2
+l_x,y = ((r,y x + a) (r^2 + a^2) - (r x + a y) (2 r r,y)) / (r^2 + a^2)^2
+l_x,z = ((r,z x    ) (r^2 + a^2) - (r x + a y) (2 r r,z)) / (r^2 + a^2)^2
+l_y,x = ((r,x y - a) (r^2 + a^2) - (r y - a x) (2 r r,x)) / (r^2 + a^2)^2
+l_y,y = ((r,y y + r) (r^2 + a^2) - (r y - a x) (2 r r,y)) / (r^2 + a^2)^2
+l_y,z = ((r,z y    ) (r^2 + a^2) - (r y - a x) (2 r r,z)) / (r^2 + a^2)^2
+l_z,x = -z r,x / r^2
+l_z,y = -z r,y / r^2
+l_z,z = (r - z r,z) / r^2
+
+
+H = .5 (r rs - Q^2) / (r^2 + a^2 z^2/r^2)
+H_,t = 0
+H_,i = (
+		.5 r_,i rs (r^2 + a^2 z^2 / r^2) 
+		- (r rs - Q^2) (r r_,i + a^2 (delta_zi r - z r_,i) z / r^3)
+	) / (r^2 + a^2 z^2 / r^2)^2
+
+g_uv = eta_uv + 2 H l_u l_v
+g_tt = -1 + 2 H
+g_ti = 2 H l_i
+g_ij = delta_ij + 2 H l_i l_j
+
+g^uv = eta^uv - 2 H (eta^ua l_a) (eta^vb l_b)
+g^tt = -1 - 2 H
+g^ti = 2 H l_i
+g^ij = delta^ij - 2 H l_i l_j
+
+g_uv,w = 2 (H,w l_u l_v + H (l_u,w l_v + l_u l_v,w))
+g_uv,t = 0
+g_tt,k = 2 H_,k
+g_ti,k = 2 l_i H_,k + 2 H l_i,k
+g_ij,k = 2 (H,k l_i l_j + H (l_i,k l_j + l_i l_j,k))
+
+Conn_uvw = 1/2 (g_uv,w + g_uw,v - g_wv,u)
+	= H,w l_u l_v + H (l_u,w l_v + l_u l_v,w)
+		+ H,v l_u l_w + H (l_u,v l_w + l_u l_w,v)
+		- H,u l_w l_v - H (l_w,u l_v + l_w l_v,u)
+	= H,w l_u l_v + H,v l_u l_w - H,u l_w l_v 
+		+ H (
+			l_u (l_v,w + l_w,v)
+			+ l_v (l_u,w - l_w,u)
+			+ l_w (l_u,v - l_v,u)
+		)
+
+Conn_ttt = 
 */
 
 
@@ -198,81 +269,46 @@ uniform float blackHoleMass;
 uniform float blackHoleCharge;
 uniform float blackHoleAngularMomentum;
 
-float calc_Kerr_rSq(vec3 pos) {
-	float a = blackHoleAngularMomentum / blackHoleMass;
-	float z = pos.z;
-	float b = a*a - dot(pos,pos);
-	float rSq = .5*(-b + sqrt(b*b + 4.*a*a*z*z));
-	return rSq;
-}
+struct metricInfo_t {
+	vec4 pos;
+	float rSq;
+	float r;
+	float H;
+	vec3 l;
+};
 
-float calc_Kerr_H(vec3 pos, float r) {
+metricInfo_t init_metricInfo(vec4 pos) {
+	metricInfo_t m;
+	m.pos = pos;
 	float rs = 2. * blackHoleMass;
+	float a = blackHoleAngularMomentum / blackHoleMass;
+	float aSq = a * a;
 	float Q = blackHoleCharge;
-	float a = blackHoleAngularMomentum / blackHoleMass;
-	float z = pos.z;
-	float H = .5 * (r * rs - Q * Q) / (r*r + a*a*z*z/(r*r));
-	return H;
+	float zSq = pos.z * pos.z;
+	float b = aSq - dot(pos.xyz, pos.xyz);
+	m.rSq = .5 * (-b + sqrt(b * b + 4. * aSq * zSq));
+	m.r = sqrt(m.rSq);
+	m.H = .5 * (m.r * rs - Q * Q) / (m.rSq + aSq * zSq / m.rSq);
+	m.l = vec3(
+		(m.r * pos.x + a * pos.y) / (m.rSq + aSq),
+		(m.r * pos.y - a * pos.x) / (m.rSq + aSq),
+		pos.z / m.r);
+	return m;
 }
 
-vec3 calc_Kerr_l(vec3 pos, float r) {
-	float a = blackHoleAngularMomentum / blackHoleMass;
-	float x = pos.x;
-	float y = pos.y;
-	float z = pos.z;
-	vec3 l = vec3(
-		(r*x + a*y)/(r*r + a*a),
-		(r*y - a*x)/(r*r + a*a),
-		z/r);
-	return l;
+float g_tt(metricInfo_t m) {
+	return -1. + 2. * m.H;
 }
 
-float g_tt(vec3 pos) {
-	float r = sqrt(calc_Kerr_rSq(pos));
-	float H = calc_Kerr_H(pos, r);
-	return -1. + 2. * H;
+vec3 g_ti(metricInfo_t m) { 
+	return 2. * m.H * m.l;
 }
 
-vec3 g_ti(vec3 pos) { 
-	float r = sqrt(calc_Kerr_rSq(pos));
-	float H = calc_Kerr_H(pos, r);
-	vec3 l = calc_Kerr_l(pos, r);
-	return 2. * H * l;
+mat3 g_ij(metricInfo_t m) {
+	return mat3(1.) + 2. * m.H * outerProduct(m.l, m.l);
 }
 
-mat3 g_ij(vec3 pos) {
-	float r = sqrt(calc_Kerr_rSq(pos));
-	float H = calc_Kerr_H(pos, r);
-	vec3 l = calc_Kerr_l(pos, r);
-	return mat3(1.) + 2. * H * outerProduct(l,l);
-}
-
-//
-//l_t = 1
-//l_x = (r x + a y) / (r^2 + a^2)
-//l_y = (r y - a x) / (r^2 + a^2)
-//l_z = z / r
-//
-//H = .5 (r rs - Q^2) / (r^2 + a^2 z^2/r^2)
-//H,i = 
-//	(
-//		.5 r_,i rs (r^2 + a^2 z^2 / r^2) 
-//		- (r rs - Q^2) (r r_,i + a^2 (delta_zi r - z r_,i) z / r^3)
-//	) / (r^2 + a^2 z^2 / r^2)^2
-//
-//g_uv = eta_uv + 2 H l_u l_v
-//g_tt = -1 + 2 H
-//g_ti = 2 H l_i
-//g_ij = delta_ij + 2 H l_i l_j
-//
-//g^uv = eta^uv - 2 H (eta^ua l_a) (eta^vb l_b)
-//g^tt = -1 - 2 H
-//g^ti = 2 H l_i
-//g^ij = delta^ij - 2 H l_i l_j
-//
-//g_uv,w = 2 (H,w l_u l_v + H (l_u,w l_v + l_u l_v,w))
-
-vec4 accel(vec4 pos, vec4 vel) {
+vec4 accel(metricInfo_t m, vec4 pos, vec4 vel) {
 #if 1
 	float t = pos.w;
 	float x = pos.x;
@@ -286,17 +322,19 @@ vec4 accel(vec4 pos, vec4 vel) {
 	float Q = blackHoleCharge;
 	float QSq = Q * Q;
 	float a = blackHoleAngularMomentum / blackHoleMass;
-	float aSq = a*a;
-	float b = aSq - pos_pos;
-	float sqrtdiscr = sqrt(b*b + 4.*aSq*zSq);
-	float rSq = .5*(-b + sqrtdiscr);
-	float r = sqrt(rSq);
-
+	float aSq = a * a;
+	
+	float rSq = m.rSq;
+	float r = m.r;
+	
 	vec3 zHat = vec3(0., 0., 1.);
-	vec3 dr_dx = (pos.xyz * r + (pos.xyz + zHat) * aSq / r) / (2. * rSq + aSq - pos_pos);
+	vec3 dr_dx = (
+			pos.xyz * r 
+			+ (pos.xyz + zHat) * aSq / r
+		) / (2. * rSq + aSq - pos_pos);
 	
 	float H_denom = rSq + aSq * zSq / rSq;
-	float H = .5 * (r * rs - QSq) / H_denom;
+	float H = m.H;
 
 	vec4 dH_dx;
 	dH_dx.xyz = (
@@ -307,44 +345,24 @@ vec4 accel(vec4 pos, vec4 vel) {
 		) / (H_denom * H_denom);
 
 	float aSq_plus_rSq = rSq + aSq;
+	float aSq_plus_rSq_sq = aSq_plus_rSq * aSq_plus_rSq;  
 	
-	vec4 l = vec4(
-		(r*x + a*y)/aSq_plus_rSq,
-		(r*y - a*x)/aSq_plus_rSq,
-		z/r,
-		1.
-	);
-
-	vec4 lU = l;
-	lU.w = -lU.w;
-
-	mat4 gInv = mat4(
-		vec4(1., 0., 0., 0.),
-		vec4(0., 1., 0., 0.),
-		vec4(0., 0., 1., 0.),
-		vec4(0., 0., 0., -1.)
-	) - 2. * H * outerProduct(lU, lU);
-
 
 	// l_a,b == dl_dx[a][b]
-	mat4 dl_dx = mat4(
-		vec4(
-			(dr_dx[0] * (x * (aSq - rSq) - 2. * a * y * r) / aSq_plus_rSq + r) / aSq_plus_rSq,
-			(dr_dx[1]  * (x * (aSq - rSq) - 2. * a * y * r) / aSq_plus_rSq + a) / aSq_plus_rSq,
-			-dr_dx[2] * (x * (aSq - rSq) - 2. * a * y * r) / (aSq_plus_rSq * aSq_plus_rSq),
-			0.),
-		vec4(
-			(dr_dx[0] * (y * (aSq - rSq) + 2. * a * x * r) / aSq_plus_rSq - a) / aSq_plus_rSq,
-			(dr_dx[1] * (y * (aSq - rSq) + 2. * a * x * r) / aSq_plus_rSq + r) / aSq_plus_rSq,
-			dr_dx[2] * (y * (aSq - rSq) + 2. * a * x * r) / (aSq_plus_rSq * aSq_plus_rSq),
-			0.),
-		vec4(
-			-z * dr_dx[0] / rSq,
-			-z * dr_dx[1] / rSq,
-			1./r - z * dr_dx[2] / rSq,
-			0.),
-		vec4(0., 0., 0., 0.));
+	mat4 dl_dx;
+	dl_dx[0][0] = ((dr_dx.x * x + r) * aSq_plus_rSq - (r * x + a * y) * (2. * r * dr_dx.x)) / aSq_plus_rSq_sq;
+	dl_dx[0][1] = ((dr_dx.y * x + a) * aSq_plus_rSq - (r * x + a * y) * (2. * r * dr_dx.y)) / aSq_plus_rSq_sq;
+	dl_dx[0][2] = ((dr_dx.z * x    ) * aSq_plus_rSq - (r * x + a * y) * (2. * r * dr_dx.z)) / aSq_plus_rSq_sq;
+	dl_dx[1][0] = ((dr_dx.x * y - a) * aSq_plus_rSq - (r * y - a * x) * (2. * r * dr_dx.x)) / aSq_plus_rSq_sq;
+	dl_dx[1][1] = ((dr_dx.y * y + r) * aSq_plus_rSq - (r * y - a * x) * (2. * r * dr_dx.y)) / aSq_plus_rSq_sq;
+	dl_dx[1][2] = ((dr_dx.z * y    ) * aSq_plus_rSq - (r * y - a * x) * (2. * r * dr_dx.z)) / aSq_plus_rSq_sq;
+	dl_dx[2][0] = -z * dr_dx.x / rSq;
+	dl_dx[2][1] = -z * dr_dx.y / rSq;
+	dl_dx[2][2] = (r - z * dr_dx.z) / rSq;
+
 	
+	vec4 l = vec4(m.l, 1.);
+
 	// conn_abc = H,a l_b l_c + H,b l_a l_c + H,c l_a l_b
 	//		+ H (l_a (l_b,c + l_c,b) + l_b (l_a,c + l_c,a) + l_c (l_a,b + l_b,a))
 	vec4 conn_vel_vel;
@@ -365,7 +383,17 @@ vec4 accel(vec4 pos, vec4 vel) {
 		}
 		conn_vel_vel[a] = sum;
 	}
+
 	
+	vec4 lU = vec4(m.l, -1.);
+	
+	mat4 gInv = mat4(
+		vec4(1., 0., 0., 0.),
+		vec4(0., 1., 0., 0.),
+		vec4(0., 0., 1., 0.),
+		vec4(0., 0., 0., -1.)
+	) - 2. * H * outerProduct(lU, lU);
+
 	// conn^a_bc = g^ad conn_dbc
 	return -gInv * conn_vel_vel;
 #endif
@@ -405,7 +433,18 @@ uniform float blackHoleMass;
 uniform float blackHoleCharge;
 uniform float blackHoleAngularMomentum;
 
-float g_tt(vec3 pos) {
+struct metricInfo_t {
+	vec3 pos;
+};
+
+metricInfo_t init_metricInfo(vec4 pos) {
+	metricInfo_t m;
+	m.pos = pos.xyz;
+	return m;
+}
+
+float g_tt(metricInfo_t m) {
+	vec3 pos = m.pos;
 	float rs = 2. * blackHoleMass;
 	float Q = blackHoleCharge;
 	float a = blackHoleAngularMomentum / blackHoleMass;
@@ -417,7 +456,8 @@ float g_tt(vec3 pos) {
 	return -1. + 2. * H;
 }
 
-vec3 g_ti(vec3 pos) { 
+vec3 g_ti(metricInfo_t m) { 
+	vec3 pos = m.pos;
 	float rs = 2. * blackHoleMass;
 	float Q = blackHoleCharge;
 	float a = blackHoleAngularMomentum / blackHoleMass;
@@ -435,7 +475,8 @@ vec3 g_ti(vec3 pos) {
 	return 2. * H * l;
 }
 
-mat3 g_ij(vec3 pos) {
+mat3 g_ij(metricInfo_t m) {
+	vec3 pos = m.pos;
 	float rs = 2. * blackHoleMass;
 	float Q = blackHoleCharge;
 	float a = blackHoleAngularMomentum / blackHoleMass;
@@ -453,7 +494,7 @@ mat3 g_ij(vec3 pos) {
 	return mat3(1.) + 2. * H * outerProduct(l,l);
 }
 
-vec4 accel(vec4 pos, vec4 vel) {
+vec4 accel(metricInfo_t m, vec4 pos, vec4 vel) {
 	float t = pos.w;
 	float x = pos.x;
 	float y = pos.y;
@@ -565,76 +606,68 @@ uniform float warpBubbleThickness;
 uniform float warpBubbleVelocity;
 uniform float warpBubbleRadius;
 
-float Alcubierre_f(vec3 pos) {
-	float rs = length(pos);
-	float sigmaFront = warpBubbleThickness * (rs + warpBubbleRadius);
-	float sigmaCenter = warpBubbleThickness * warpBubbleRadius;
-	float sigmaBack = warpBubbleThickness * (rs - warpBubbleRadius);
-	float tanhSigmaCenter = tanh(sigmaCenter);
-	float f = (tanh(sigmaFront) - tanh(sigmaBack)) / (2. * tanhSigmaCenter);
-	return f;
+struct metricInfo_t {
+	float r;
+	float sigmaFront;
+	float sigmaCenter;
+	float sigmaBack;
+	float tanhSigmaCenter;
+	float f;
+};
+
+metricInfo_t init_metricInfo(vec4 pos) {
+	metricInfo_t m;
+	m.r = length(pos.xyz);
+	m.sigmaFront = warpBubbleThickness * (m.r + warpBubbleRadius);
+	m.sigmaCenter = warpBubbleThickness * warpBubbleRadius;
+	m.sigmaBack = warpBubbleThickness * (m.r - warpBubbleRadius);
+	m.tanhSigmaCenter = tanh(m.sigmaCenter);
+	m.f = (tanh(m.sigmaFront) - tanh(m.sigmaBack)) / (2. * m.tanhSigmaCenter);
+	return m;
 }
 
-float g_tt(vec3 pos) {
-	float f = Alcubierre_f(pos);
-	float vf = f * warpBubbleVelocity;
+float g_tt(metricInfo_t m) {
+	float vf = m.f * warpBubbleVelocity;
 	float vf2 = vf * vf;
-	return -(1. - vf2); 
+	return -1. + vf2; 
 }
 
-vec3 g_ti(vec3 pos) {
-	float f = Alcubierre_f(pos);
-	float vf = f * warpBubbleVelocity;
-	float g_tx = -vf;
-	return vec3(g_tx, 0., 0.);
+vec3 g_ti(metricInfo_t m) {
+	return vec3(-m.f * warpBubbleVelocity, 0., 0.);
 }
 
-mat3 g_ij(vec3 pos) {
+mat3 g_ij(metricInfo_t m) {
 	return mat3(1.);
 }
 
-vec4 accel(vec4 pos, vec4 vel) {
-	float r = length(pos.xyz);
-	float sigmaFront = warpBubbleThickness * (r + warpBubbleRadius);
-	float sigmaCenter = warpBubbleThickness * warpBubbleRadius;
-	float sigmaBack = warpBubbleThickness * (r - warpBubbleRadius);
-	float tanhSigmaCenter = tanh(sigmaCenter);
-	float f = (tanh(sigmaFront) - tanh(sigmaBack)) / (2. * tanhSigmaCenter);
-	float sechDiff = sechSq(sigmaFront) - sechSq(sigmaBack);
-	float dfScalar = sechDiff / (2. * r * tanhSigmaCenter);
+vec4 accel(metricInfo_t m, vec4 pos, vec4 vel) {
+	float sechDiff = sechSq(m.sigmaFront) - sechSq(m.sigmaBack);
+	float dfScalar = sechDiff / (2. * m.r * m.tanhSigmaCenter);
+	
 	vec4 df;
 	df.xyz = warpBubbleThickness * pos.xyz * dfScalar;
 	df.w = -warpBubbleVelocity * warpBubbleThickness * pos.x * dfScalar;
 
-	float u = f * warpBubbleVelocity;
+	float u = m.f * warpBubbleVelocity;
 	vec4 du = df * warpBubbleVelocity;
-	
+
+	float du_v_dot3 = dot(du.xyz, vel.xyz);
+	float du_v_dot4 = dot(du, vel);
+
 	vec4 result;
 	result.w = 
-		- du.x * vel.x * vel.x
-		- du.y * vel.x * vel.y
-		- du.z * vel.x * vel.z
-		
-		+ 2. * du.x * vel.w * vel.x * u
-		- du.x * vel.w * vel.w * u * u
-		+ du.y * vel.w * vel.y * u
-		+ du.z * vel.w * vel.z * u
+		- vel.x * du_v_dot3
+		+ u * vel.w * (du.x * vel.x + du_v_dot3)
+		- u * u * vel.w * vel.w * du.x
 	;
-	result.x = 
-		du.w * vel.w * vel.w
-		+ du.y * vel.w * vel.y
-		+ du.y * vel.w * vel.y * u * u
-		- du.y * vel.x * vel.y * u
-		+ du.z * vel.w * vel.z
-		+ du.z * vel.w * vel.z * u * u
-		- du.z * vel.x * vel.z * u
-		+ du.x * vel.w * vel.w * u
-		- du.x * vel.w * vel.w * u * u * u
-		+ 2. * du.x * vel.w * vel.x * u * u
-		- du.x * vel.x * vel.x * u
-	;
-	result.y = -du.y * vel.w * (vel.x - vel.w * u);
-	result.z = -du.z * vel.w * (vel.x - vel.w * u);
+	result.xyz = -du.xyz * vel.w * (vel.x - vel.w * u);
+	result.x += 
+		- vel.x * u * du_v_dot3
+		+ vel.w * (
+			du_v_dot4
+			+ u * u * (du.x * vel.x + du_v_dot3)
+			- vel.w * du.x * u * u * u
+		);
 	return result;
 }
 
@@ -688,26 +721,27 @@ void main() {
 //(v^t)^2 + (2 v^i g_ti / g_tt) v^t + g_ij v^i v^j / g_tt = 0
 //v_t = v^i g_ti / (-g_tt) +- sqrt( (v^i g_ti / g_tt)^2 + g_ij v^i v^j / (-g_tt) )
 // I'll go with the plus
-float reset_w(vec3 pos, vec3 vel) {
-	float _g_tt = g_tt(pos);
+float reset_w(vec4 pos, vec4 vel) {
+	metricInfo_t m = init_metricInfo(pos);
+	float _g_tt = g_tt(m);
 	float negInv_g_tt = -1. / _g_tt;
-	vec3 _g_ti = g_ti(pos);
-	float v_t = dot(vel, _g_ti);	//v_t = g_ti v^i
+	vec3 _g_ti = g_ti(m);
+	float v_t = dot(vel.xyz, _g_ti);	//v_t = g_ti v^i
 	float a = v_t * negInv_g_tt;
-	float bsq = a * a + dot(vel, g_ij(pos) * vel) * negInv_g_tt;
+	float bsq = a * a + dot(vel.xyz, g_ij(m) * vel.xyz) * negInv_g_tt;
 	if (bsq < 0.) bsq = 0.;
 	return a + sqrt(bsq);
 }
 
 varying vec3 vtxv;
 void main() {
-	vec3 pos = vec3(-objectDist, 0., 0.);
-	pos = quatRotate(objectAngle, pos);
+	vec4 pos = vec4(-objectDist, 0., 0., 0.);
+	pos.xyz = quatRotate(objectAngle, pos.xyz);
 	
-	vec3 vel = normalize(vtxv);
-	vel = quatRotate(objectAngle, vel);
+	vec4 vel = vec4(normalize(vtxv.xyz), 0.);
+	vel.xyz = quatRotate(objectAngle, vel.xyz);
 	
-	gl_FragColor.xyz = vel;
+	gl_FragColor.xyz = vel.xyz;
 	gl_FragColor.w = reset_w(pos, vel);
 }
 */}));
@@ -760,7 +794,7 @@ void main() {
 	float distSq = dot(pos.xyz, pos.xyz);
 	gl_FragColor = vel;
 	if (distSq < maxRadius) {
-		gl_FragColor += deltaLambda * accel(pos, vel);
+		gl_FragColor += deltaLambda * accel(init_metricInfo(pos), pos, vel);
 	}
 }
 */}));
